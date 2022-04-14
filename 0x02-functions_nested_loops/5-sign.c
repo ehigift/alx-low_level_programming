9e52497f5592317119a1bdf94a06ae8b0223052b
Ehi Okoh <ehigift4@gmail.com>
2022-04-14 03:53:11 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -12,16 +13,19 @@ int test;
 
 if (n > 0)
 {
-        test = 1;
-        _putchar('+');
+	test = 1;
+	_putchar('+');
 }
 else if (n == 0)
 {
-        test = 0;
-        _putchar('0');
+	test = 0;
+	_putchar('0');
 }
 else
 {
-        test = -1;
-        _putchar('-');
+	test = -1;
+	_putchar('-');
+}
+
+return (test);
 }
