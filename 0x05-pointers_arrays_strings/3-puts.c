65664bf89c95ed6c316e8cf53fec0247f5567399
Ehi Okoh <ehigift4@gmail.com>
2022-05-16 02:08:00 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,12 @@
  */
 void _puts(char *str)
 {
-        int i;
+	int i;
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                _putchar(str[i]);
-        }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 _putchar('\n');
 return;
 }
